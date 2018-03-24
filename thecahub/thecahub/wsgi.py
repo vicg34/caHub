5c85867b084a65c89e8636d514c7bcdb35f7090e
Vicky <vickygupta24d93@gmail.com>
2018-03-24 17:09:39 +0530

    set environment

--- thecahub/thecahub/wsgi.py
+++ thecahub/thecahub/wsgi.py
@@ -7,7 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+sys.path.append('/home/ubuntu/CaHub/caHub/thecahub/')
+sys.path.append('/home/ubuntu/CaHub/env/bin/python')
 
 from django.core.wsgi import get_wsgi_application
 
